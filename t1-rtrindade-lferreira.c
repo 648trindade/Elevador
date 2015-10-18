d6e12f05f246365176d1c298c555030b0470a5de
Rafael <rtrindade@inf.ufsm.br>
2015-10-18 16:37:09 -0200

    18/10

--- t1-rtrindade-lferreira.c
+++ t1-rtrindade-lferreira.c
@@ -5,7 +5,7 @@
 #include <time.h>
 
 pthread_mutex_t mutex_pedidos;
-pthread_cond_t cond;
+
 clock_t ini;
 int bi = 1e9;
 
@@ -14,7 +14,7 @@ int bi = 1e9;
 	Contém uma variável (status) indicando o estado da pessoa: 
 	*	0:	Visitando o andar;
 	*	1:	Dentro do elevador;
-	*  -1:	Esperando o elevador;
+	*   2:	Esperando o elevador;
 */
 typedef struct{
 	int id;				// Número da pessoa;
@@ -22,6 +22,8 @@ typedef struct{
 	int status;			// Estado atual da pessoa;
 	lista_t dest;		// Vetor de destinos da pessoa;
 	lista_t time;		// Vetor de tempo de visita em cada andar destino;
+	pthread_cond_t c;
+	pthread_mutex_t m;
 }pessoa;
 
 /*	=== Elevador ===
@@ -54,56 +56,61 @@ void chama_elevador(pessoa *p, int dest){
 	int sentido;
 	if (dest > p->andar){
 		sentido = 0; 	//pessoa vai subir
-		printf("%d %d S %d\n",p->id,(int)((clock()-ini)*bi/CLOCKS_PER_SEC),p->andar);
+		//printf("%d %d S %d\n",p->id,(int)((clock()-ini)*bi/CLOCKS_PER_SEC),p->andar);
+		printf("%d %d S %d\n",p->id,(int)(clock()-ini),p->andar);
 	}
 	else{
 		sentido = 1; 	//pessoa vai descer
-		printf("%d %d D %d\n",p->id,(int)((clock()-ini)*bi/CLOCKS_PER_SEC),p->andar);
+		//printf("%d %d D %d\n",p->id,(int)((clock()-ini)*bi/CLOCKS_PER_SEC),p->andar);
+		printf("%d %d D %d\n",p->id,(int)(clock()-ini),p->andar);
 	}
 	l_insere_ord(&(e.pedidos[sentido]), dest, sentido);
 	pthread_mutex_unlock(&mutex_pedidos);
+	p->status = 1;
 }
 
 void *acao_pessoa(void *arg){
 	pessoa *p = (pessoa *) arg;
+	printf("%d %d E 0\n",p->id,(int)(clock()-ini));
 	int p_dest, p_temp;
+	struct timespec wait, rest;
+	wait.tv_sec = 0;
 	while (1) {
 		// consulta proximo destino
 		p_dest = l_retira(&(p->dest));
-		p_temp = l_retira(&(p->time));
 		// chama o elevador
 		chama_elevador(p, p_dest);
+		//espera ser acordado pelo elevador (elevador chegar no andar)
+		pthread_mutex_lock(&(p->m));
+		pthread_cond_wait(&(p->c),&(p->m));
+		pthread_mutex_unlock(&(p->m));
+		//entra elevador
 		//TODO
-		//pthread_cond_
-		
-		
-		
+		p->andar = p_dest;
+		p->status = 2;
+		//espera ser acordado pelo elevador (elevador chegar no destino)
+		pthread_mutex_lock(&(p->m));
+		pthread_cond_wait(&(p->c),&(p->m));
+		pthread_mutex_unlock(&(p->m));
+		//entra no andar e visita (espera)
+		p->status = 0;
+		wait.tv_nsec = (long) l_retira(&(p->time));
+		nanosleep(&wait, &rest);
+		//TODO
 		if (p->dest.qtd == 0)
 			break;
 	}
 	pthread_exit(NULL);
 }
 
-int main(){
-	pthread_mutex_init(&mutex_pedidos, 0);
-	pthread_cond_init(&cond, 0);
-	//Lendo
-	int n,j,i,d,a,t;
-	scanf("%d",&n);
-	pessoa p[n];
-	// Descreve elevador
-	e.qtd_p = 0;
-	e.andar = 0;
-	l_cria(&(e.dest));
-	l_cria(&(e.pedidos[0]));
-	l_cria(&(e.pedidos[1]));
-	e.status = 0;	// Parado
-	//Lê pessoas
-	pthread_t p_thr[n], e_thr;
-	ini = clock();
+void cria_threads(pessoa p[], pthread_t p_thr[], int n){
+	int i, j, d, a, t;
 	for (i=0;i<n;i++){
+		//cria condicional
 		scanf("%d",&d);
-		p[i].id = i+100;
+		pthread_cond_init(&(p[i].c), 0);
+		pthread_mutex_init(&(p[i].m), 0);
+		p[i].id = i;
 		p[i].andar = 0;
 		p[i].status = 0;
 		l_cria(&(p[i].time));
@@ -116,21 +123,40 @@ int main(){
 		l_insere(&(p[i].dest), 0);
 		l_insere(&(p[i].time),-1);
 		pthread_create(&p_thr[i], NULL, acao_pessoa, (void*)&p[i]);
-		printf("%d %d E 0\n",p[i].id,(int)((clock()-ini)*bi/CLOCKS_PER_SEC));
+		//printf("%d %d E 0\n",p[i].id,(int)((clock()-ini)*bi/CLOCKS_PER_SEC));
 	}
+}
+
+int main(){
+	pthread_mutex_init(&mutex_pedidos, 0);
+	//Lendo
+	int n,j,i;
+	scanf("%d",&n);
+	pessoa p[n];
+	// Descreve elevador
+	e.qtd_p = 0;
+	e.andar = 0;
+	l_cria(&(e.dest));
+	l_cria(&(e.pedidos[0]));
+	l_cria(&(e.pedidos[1]));
+	e.status = 0;	// Parado
+	//Lê pessoas
+	pthread_t p_thr[n], e_thr;
+	ini = clock();
+	cria_threads(p, p_thr, n);
 	
 	for (i=0;i < n; i++){
 		pthread_join(p_thr[i],0);
+		pthread_cond_destroy(&(p[i].c));
+		pthread_mutex_destroy(&(p[i].m));
 	}
 	printf("JOIN!\n");
 	
-	t = e.pedidos[0].qtd;
-	printf("%d\n",t);
-	for (i=0;i < t; i++){
+	j= e.pedidos[0].qtd;
+	for (i=0;i < j; i++){
 		printf("%d ",l_retira(&(e.pedidos[0])));
 	}printf("\n");
 	pthread_mutex_destroy(&mutex_pedidos);
-	pthread_cond_destroy(&cond);
 	pthread_exit(0);
 	return 0;
 }
